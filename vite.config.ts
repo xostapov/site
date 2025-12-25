3c05c51254637f8c82274bf32fe9198fdaf4557d
xostapov <antonchikhah@outlook.com>
2025-12-25 06:18:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ВАЖНО: замени 'xostapov' на имя твоего репозитория на GitHub!
+  base: mode === "production" ? "/site/" : "/",
   server: {
     host: "::",
     port: 8080,
