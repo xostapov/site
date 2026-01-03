be6c59fba3c9a0fc634b36c535f5bd83f8ffaf03
xostapov <antonchikhah@outlook.com>
2026-01-03 11:36:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // ВАЖНО: замени 'xostapov' на имя твоего репозитория на GitHub!
-  base: mode === "production" ? "/site/" : "/",
+  base: "/", // 🔥 ВСЕГДА /
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
